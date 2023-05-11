78828b77287933e2c548fb98dde2cb05afaad4bb
sibkit <sibkit@hotmail.com>
2023-05-11 17:10:32 +0700

    repair PgSqlDriver

--- src/juke/postgresql/PgSqlDriver.java
+++ src/juke/postgresql/PgSqlDriver.java
@@ -3,12 +3,8 @@ package juke.postgresql;
 import juke.jdbc.JdbcDriver;
 import juke.jdbc.sqlbuilding.SqlBuilder;
 
-public class PgSqlDriver {
-    JdbcDriver jdbcDriver;
+public class PgSqlDriver extends JdbcDriver {
     public PgSqlDriver() {
-        jdbcDriver = new JdbcDriver();
-        jdbcDriver.setSqlBuilder(new PgSqlBuilder());
+        super.setSqlBuilder(new PgSqlBuilder());
     }
-
-
 }
